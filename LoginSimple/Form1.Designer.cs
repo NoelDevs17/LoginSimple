8d2bcced926bddf1616195ae11ae9f5e21cda6ac
Noel Ortiz <67615585+NoelDevs17@users.noreply.github.com>
2021-06-29 23:21:32 -0400

    change background,  font the label and button

--- LoginSimple/Form1.Designer.cs
+++ LoginSimple/Form1.Designer.cs
@@ -29,23 +29,27 @@ namespace LoginSimple
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.txtUser = new System.Windows.Forms.TextBox();
             this.txtPassword = new System.Windows.Forms.TextBox();
             this.lblUser = new System.Windows.Forms.Label();
             this.lblPass = new System.Windows.Forms.Label();
             this.lblLog = new System.Windows.Forms.Label();
+            this.btnAcces = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCreateUser = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // txtUser
             // 
-            this.txtUser.Location = new System.Drawing.Point(101, 128);
+            this.txtUser.Location = new System.Drawing.Point(105, 128);
             this.txtUser.Name = "txtUser";
             this.txtUser.Size = new System.Drawing.Size(179, 23);
             this.txtUser.TabIndex = 0;
             // 
             // txtPassword
             // 
-            this.txtPassword.Location = new System.Drawing.Point(101, 157);
+            this.txtPassword.Location = new System.Drawing.Point(105, 157);
             this.txtPassword.Name = "txtPassword";
             this.txtPassword.Size = new System.Drawing.Size(179, 23);
             this.txtPassword.TabIndex = 1;
@@ -54,20 +58,22 @@ namespace LoginSimple
             // lblUser
             // 
             this.lblUser.AutoSize = true;
-            this.lblUser.Font = new System.Drawing.Font("Segoe UI Historic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblUser.BackColor = System.Drawing.Color.Transparent;
+            this.lblUser.Font = new System.Drawing.Font("Segoe UI Historic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.lblUser.Location = new System.Drawing.Point(45, 123);
             this.lblUser.Name = "lblUser";
-            this.lblUser.Size = new System.Drawing.Size(54, 25);
+            this.lblUser.Size = new System.Drawing.Size(59, 25);
             this.lblUser.TabIndex = 2;
             this.lblUser.Text = "User:";
             // 
             // lblPass
             // 
             this.lblPass.AutoSize = true;
-            this.lblPass.Font = new System.Drawing.Font("Segoe UI Historic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblPass.BackColor = System.Drawing.Color.Transparent;
+            this.lblPass.Font = new System.Drawing.Font("Segoe UI Historic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.lblPass.Location = new System.Drawing.Point(3, 155);
             this.lblPass.Name = "lblPass";
-            this.lblPass.Size = new System.Drawing.Size(96, 25);
+            this.lblPass.Size = new System.Drawing.Size(105, 25);
             this.lblPass.TabIndex = 3;
             this.lblPass.Text = "Password:";
             this.lblPass.Click += new System.EventHandler(this.label2_Click);
@@ -75,18 +81,57 @@ namespace LoginSimple
             // lblLog
             // 
             this.lblLog.AutoSize = true;
-            this.lblLog.Font = new System.Drawing.Font("Segoe UI Historic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lblLog.Location = new System.Drawing.Point(144, 83);
+            this.lblLog.BackColor = System.Drawing.Color.Transparent;
+            this.lblLog.Font = new System.Drawing.Font("Segoe UI Historic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblLog.Location = new System.Drawing.Point(140, 83);
             this.lblLog.Name = "lblLog";
-            this.lblLog.Size = new System.Drawing.Size(80, 32);
+            this.lblLog.Size = new System.Drawing.Size(86, 32);
             this.lblLog.TabIndex = 4;
             this.lblLog.Text = "Log in";
+            // 
+            // btnAcces
+            // 
+            this.btnAcces.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnAcces.Font = new System.Drawing.Font("Segoe UI Historic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnAcces.Location = new System.Drawing.Point(105, 186);
+            this.btnAcces.Name = "btnAcces";
+            this.btnAcces.Size = new System.Drawing.Size(179, 38);
+            this.btnAcces.TabIndex = 5;
+            this.btnAcces.Text = "Access";
+            this.btnAcces.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Historic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(12, 236);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(294, 25);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "¿You do not have an account?";
+            // 
+            // btnCreateUser
+            // 
+            this.btnCreateUser.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnCreateUser.Font = new System.Drawing.Font("Segoe UI Historic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnCreateUser.Location = new System.Drawing.Point(105, 276);
+            this.btnCreateUser.Name = "btnCreateUser";
+            this.btnCreateUser.Size = new System.Drawing.Size(179, 34);
+            this.btnCreateUser.TabIndex = 7;
+            this.btnCreateUser.Text = "Create User";
+            this.btnCreateUser.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(324, 364);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(338, 373);
+            this.Controls.Add(this.btnCreateUser);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnAcces);
             this.Controls.Add(this.lblLog);
             this.Controls.Add(this.lblPass);
             this.Controls.Add(this.lblUser);
@@ -106,6 +151,9 @@ namespace LoginSimple
         private System.Windows.Forms.Label lblUser;
         private System.Windows.Forms.Label lblPass;
         private System.Windows.Forms.Label lblLog;
+        private System.Windows.Forms.Button btnAcces;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCreateUser;
     }
 }
 
